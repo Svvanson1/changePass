deb2e44f8fb9f0f33ba37d6fc2e970d0323891f9
Ben Swanson <benswanson127@gmail.com>
2017-03-27 16:46:04 -0600

    Create chngpswd.sh

---
+++ chngpswd.sh
@@ -0,0 +1,26 @@
+passfile=passwords.txt
+passfile_csv=passwords.csv
+
+list="alpha bravo charlie"
+list=`awk -F':' '{ if ( $7 !~ "nologin" && $7 !~ "false" ) print $1 }' "/etc/passwd"`
+
+rm -f $passfile
+rm -f $passfile_csv
+
+for user in $list
+  do
+    if  ! grep -w ^$user /etc/passwd > /dev/null
+    then
+      echo "user NOT present: $user"
+    else
+      read -p "Include $user in password change list? [y/n]" -n 1 -r
+      echo ""
+      if [[ $REPLY =~ ^[Yy]$ ]]
+      then
+        pass=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c16)
+        echo "$user:$pass">>$passfile
+        echo "$user,$pass">>$passfile_csv
+      fi
+    fi
+done
+cat $passfile | chpasswd
